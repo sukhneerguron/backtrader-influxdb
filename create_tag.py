a9e74c53bdf8f0ade8f80d127d7300494ad7f499
Sukhneer Singh Guron <sukhneer@seynse.com>
2023-09-21 00:00:15 +0530

    Added scaffolding for python package

---
+++ create_tag.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+__author__ = 'TheArchitect'
+
+
+def read_version():
+    with open(os.path.join('backtrader_influxdb', '__init__.py')) as f:
+        m = re.search(r'''__version__\s*=\s*['"]([^'"]*)['"]''', f.read())
+        if m:
+            return m.group(1)
+        raise ValueError("couldn't find version")
+
+
+def create_tag():
+    from subprocess import call
+    version = read_version()
+    errno = call(['git', 'tag', '--annotate', version, '--message', 'Version %s' % version])
+    if errno == 0:
+        print("Added tag for version %s" % version)
+
+
+if __name__ == '__main__':
+    create_tag()
